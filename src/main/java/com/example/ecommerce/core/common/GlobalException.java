1255901f29cf99e08f8b7c865a987dcc3cc10ca4
JEONG-BD <dev.byeongdu@gmail.com>
2025-09-23 15:53:01 +0900

    httpmessageconverter

--- src/main/java/com/example/ecommerce/core/common/GlobalException.java
+++ src/main/java/com/example/ecommerce/core/common/GlobalException.java
@@ -1,7 +1,26 @@
 package com.example.ecommerce.core.common;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 public class GlobalException extends ResponseEntityExceptionHandler{
 
+    private boolean stackTrace;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public final ErrorResponse handleBadRequestException(Exception ex, WebRequest request){
+        List<StackTraceElement> stackTraces = null;
+        if (stackTrace){
+            stackTraces = Arrays.asList(ex.getStackTrace());
+        }
+        return new ErrorResponse(stackTraces, ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
